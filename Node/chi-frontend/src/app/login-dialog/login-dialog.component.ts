aa2abb0425a576df1814af37b4762eaa820bab9a
Curt-iss <tanner9@vt.edu>
2019-11-20 12:10:19 -0500

    added environment import

--- Node/chi-frontend/src/app/login-dialog/login-dialog.component.ts
+++ Node/chi-frontend/src/app/login-dialog/login-dialog.component.ts
@@ -2,20 +2,22 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { Router } from '@angular/router';
 
+//Import current environment variables
+import { environment } from '../../environments/environment';
+
 @Component({
   selector: 'app-login-dialog',
   templateUrl: './login-dialog.component.html',
   styleUrls: ['./login-dialog.component.sass'],
 })
 export class LoginDialogComponent implements OnInit {
-
   inProgress: boolean;
 
   //TODO Interface for data parameter
   constructor(
     public dialogRef: MatDialogRef<LoginDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private router: Router
+    private router: Router,
   ) {}
 
   ngOnInit() {
@@ -24,8 +26,8 @@ export class LoginDialogComponent implements OnInit {
 
   async login(): Promise<any> {
     this.inProgress = true;
-    console.log(this.data)
-    await this.sleep(1500);
+    console.log(this.data);
+    if (!environment.production) await this.sleep(1500);
     this.inProgress = false;
     this.dialogRef.close();
   }
@@ -34,8 +36,7 @@ export class LoginDialogComponent implements OnInit {
     this.dialogRef.close();
   }
 
-
   //Sleep helper
   //! For demo only
-  sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+  sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 }
